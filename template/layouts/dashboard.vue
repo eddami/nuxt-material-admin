291f53baca566d92f71a8c67dbf397375011d24d
Mohamed Eddami <eddamimo@gmail.com>
2019-01-30 00:46:04 +0100

    typo

--- template/layouts/dashboard.vue
+++ template/layouts/dashboard.vue
@@ -14,7 +14,7 @@
           <v-footer height="auto" class="white pa-3 app--footer">
             <span class="caption">&copy; {{ new Date().getFullYear() }}</span>
             <v-spacer></v-spacer>
-            <span class="caption mr-1"> Make With Love </span>
+            <span class="caption mr-1"> Made with love </span>
             <v-icon color="pink" small>favorite</v-icon>
           </v-footer>
         </v-content>
