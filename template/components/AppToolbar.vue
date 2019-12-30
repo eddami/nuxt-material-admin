a54ea8e705c25bd7d8abeb7fda666f54b90f4759
Mohamed Eddami <eddamimo@gmail.com>
2019-12-30 23:01:16 +0100

    fix: upgrade toolbar

--- template/components/AppToolbar.vue
+++ template/components/AppToolbar.vue
@@ -1,21 +1,15 @@
 <template>
-  <v-toolbar
-    color="primary"
-    fixed
-    dark
-    app
-  >
+  <v-app-bar color="primary" fixed dark app>
     <v-toolbar-title class="ml-0 pl-3">
-      <v-toolbar-side-icon @click.stop="toggleDrawer()"></v-toolbar-side-icon>
+      <v-app-bar-nav-icon @click.stop="toggleDrawer()"></v-app-bar-nav-icon>
     </v-toolbar-title>
     <v-text-field
       flat
       solo-inverted
       prepend-icon="search"
       label="Search"
-      class="hidden-sm-and-down"
-    >
-    </v-text-field>
+      class="hidden-sm-and-down mt-auto"
+    ></v-text-field>
     <v-spacer></v-spacer>
     <v-btn icon href="https://github.com/moeddami/nuxt-material-admin">
       <v-icon>fa-2x fa-github</v-icon>
@@ -23,7 +17,14 @@
     <v-btn icon @click="handleFullScreen()">
       <v-icon>fullscreen</v-icon>
     </v-btn>
-    <v-menu offset-y origin="center center" class="elelvation-1" :nudge-right="140" :nudge-bottom="14" transition="scale-transition">
+    <v-menu
+      offset-y
+      origin="center center"
+      class="elevation-1"
+      :nudge-right="140"
+      :nudge-bottom="14"
+      transition="scale-transition"
+    >
       <v-btn icon flat slot="activator">
         <v-badge color="red" overlap>
           <span slot="badge">3</span>
@@ -32,79 +33,97 @@
       </v-btn>
       <notification-list></notification-list>
     </v-menu>
-    <v-menu offset-y origin="center center" :nudge-right="140" :nudge-bottom="10" transition="scale-transition">
-      <v-btn icon large flat slot="activator">
-        <v-avatar size="30px">
-          <img src="../static/avatar/man_4.jpg" alt="Michael Wang"/>
-        </v-avatar>
-      </v-btn>
+    <v-menu
+      offset-y
+      origin="center center"
+      :nudge-right="140"
+      :nudge-bottom="10"
+      transition="scale-transition"
+    >
+      <template v-slot:activator="{ on }">
+        <v-btn icon large text slot="activator" v-on="on">
+          <v-avatar size="30px">
+            <v-icon>person</v-icon>
+          </v-avatar>
+        </v-btn>
+      </template>
+
       <v-list class="pa-0">
-        <v-list-tile v-for="(item,index) in items" :to="!item.href ? { name: item.name } : null" :href="item.href"
-                     @click="item.click" ripple="ripple" :disabled="item.disabled" :target="item.target" rel="noopener"
-                     :key="index">
-          <v-list-tile-action v-if="item.icon">
+        <v-list-item
+          v-for="(item,index) in items"
+          :to="!item.href ? { name: item.name } : null"
+          :href="item.href"
+          @click="item.click"
+          ripple="ripple"
+          :disabled="item.disabled"
+          :target="item.target"
+          rel="noopener"
+          :key="index"
+        >
+          <v-list-item-action v-if="item.icon">
             <v-icon>{{ item.icon }}</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-content>
-            <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-          </v-list-tile-content>
-        </v-list-tile>
+          </v-list-item-action>
+          <v-list-item-content>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item-content>
+        </v-list-item>
       </v-list>
     </v-menu>
-  </v-toolbar>
+  </v-app-bar>
 </template>
-<script>
-  import NotificationList from '@/components/widgets/list/NotificationList';
-  import Util from '@/util';
 
-  export default {
-    name: 'app-toolbar',
-    components: {
-      NotificationList
-    },
-    data:  function () {
-      return {
-        items: [
-          {
-            icon: 'account_circle',
-            href: '#',
-            title: 'Profile',
-            click: (e) => {
-              console.log(e);
-            }
-          },
-          {
-            icon: 'settings',
-            href: '#',
-            title: 'Settings',
-            click: (e) => {
-              console.log(e);
-            }
-          },
-          {
-            icon: 'fullscreen_exit',
-            href: '#',
-            title: 'Logout',
-            click: this.handleLogout
+<script>
+import Util from '@/util'
+import NotificationList from '@/components/widgets/list/NotificationList'
+
+export default {
+  name: 'app-toolbar',
+  components: {
+    NotificationList
+  },
+  data: function() {
+    return {
+      items: [
+        {
+          icon: 'account_circle',
+          href: '#',
+          title: 'Profile',
+          click: e => {
+            console.log(e)
           }
-        ],
-      }
-    },
-    computed: {
-      toolbarColor() {
-        return this.$vuetify.options.extra.mainNav;
-      }
-    },
-    methods: {
-      toggleDrawer() {
-        this.$store.commit('toggleDrawer')
-      },
-      handleFullScreen() {
-        Util.toggleFullScreen();
-      },
-      handleLogout() {
-        this.$router.push('/login');
-      }
+        },
+        {
+          icon: 'settings',
+          href: '#',
+          title: 'Settings',
+          click: e => {
+            console.log(e)
+          }
+        },
+        {
+          icon: 'fullscreen_exit',
+          href: '#',
+          title: 'Logout',
+          click: this.handleLogout
+        }
+      ]
     }
-  };
+  },
+  computed: {
+    toolbarColor() {
+      return this.$vuetify.options.extra.mainNav
+    }
+  },
+  methods: {
+    toggleDrawer() {
+      this.$store.commit('toggleDrawer')
+    },
+    handleFullScreen() {
+      Util.toggleFullScreen()
+    },
+    handleLogout() {
+      this.$router.push('/login')
+    }
+  }
+}
 </script>
