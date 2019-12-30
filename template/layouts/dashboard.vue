6f85b687281da8a2648a7005cc91f9cfa54147f2
Mohamed Eddami <eddamimo@gmail.com>
2019-12-30 23:23:24 +0100

    feat: add v-container in dashboard layout

--- template/layouts/dashboard.vue
+++ template/layouts/dashboard.vue
@@ -5,24 +5,36 @@
         <app-drawer class="app--drawer"></app-drawer>
         <app-toolbar class="app--toolbar"></app-toolbar>
         <v-content>
-          <!-- Page Header -->
-          <page-header></page-header>
-          <div class="page-wrapper">
-            <nuxt/>
-          </div>
+          <v-container>
+            <!-- Page Header -->
+            <page-header></page-header>
+            <div class="page-wrapper">
+              <nuxt />
+            </div>
+          </v-container>
           <!-- App Footer -->
           <v-footer height="auto" class="white pa-3 app--footer">
             <span class="caption">&copy; {{ new Date().getFullYear() }}</span>
             <v-spacer></v-spacer>
-            <span class="caption mr-1"> Made with love </span>
+            <span class="caption mr-1">Made with love</span>
             <v-icon color="pink" small>favorite</v-icon>
           </v-footer>
         </v-content>
         <!-- Go to top -->
         <app-fab></app-fab>
         <!-- theme setting -->
-        <v-btn small fab dark falt fixed top="top" right="right" class="setting-fab" color="red"
-               @click="openThemeSettings">
+        <v-btn
+          small
+          fab
+          dark
+          falt
+          fixed
+          top="top"
+          right="right"
+          class="setting-fab"
+          color="red"
+          @click="openThemeSettings"
+        >
           <v-icon>settings</v-icon>
         </v-btn>
         <v-navigation-drawer
@@ -38,14 +50,7 @@
       </v-app>
     </template>
 
-
-    <v-snackbar
-      :timeout="3000"
-      bottom
-      right
-      :color="snackbar.color"
-      v-model="snackbar.show"
-    >
+    <v-snackbar :timeout="3000" bottom right :color="snackbar.color" v-model="snackbar.show">
       {{ snackbar.text }}
       <v-btn dark flat @click.native="snackbar.show = false" icon>
         <v-icon>close</v-icon>
@@ -55,52 +60,54 @@
 </template>
 
 <script>
-  import AppDrawer from '@/components/AppDrawer'
-  import AppToolbar from '@/components/AppToolbar'
-  import AppFab from '@/components/AppFab'
-  import PageHeader from '@/components/PageHeader'
-  import ThemeSettings from '@/components/ThemeSettings'
+import AppDrawer from '@/components/AppDrawer'
+import AppToolbar from '@/components/AppToolbar'
+import AppFab from '@/components/AppFab'
+import PageHeader from '@/components/PageHeader'
+import ThemeSettings from '@/components/ThemeSettings'
 
-  export default {
-    components: {
-      AppDrawer,
-      AppToolbar,
-      AppFab,
-      PageHeader,
-      ThemeSettings
-    },
-    data: () => ({
-      expanded: true,
-      rightDrawer: false,
-      snackbar: {
-        show: false,
-        text: '',
-        color: '',
-      }
-    }),
+export default {
+  components: {
+    AppDrawer,
+    AppToolbar,
+    AppFab,
+    PageHeader,
+    ThemeSettings
+  },
+  data: () => ({
+    expanded: true,
+    rightDrawer: false,
+    snackbar: {
+      show: false,
+      text: '',
+      color: ''
+    }
+  }),
 
-    methods: {
-      openThemeSettings() {
-        this.$vuetify.goTo(0)
-        this.rightDrawer = (!this.rightDrawer)
-      }
+  methods: {
+    openThemeSettings() {
+      this.$vuetify.goTo(0)
+      this.rightDrawer = !this.rightDrawer
     }
   }
+}
 </script>
 
 <style lang="stylus" scoped>
-  .setting-fab
-    top: 50% !important;
-    right: 0;
-    border-radius: 0
+.setting-fab {
+  top: 50% !important;
+  right: 0;
+  border-radius: 0;
+}
 
-  .page-wrapper
-    min-height: calc(100vh - 64px - 50px - 81px);
-    margin-bottom 50px;
+.page-wrapper {
+  min-height: calc(100vh - 64px - 50px - 81px);
+  margin-bottom: 50px;
+}
 
-  .app--footer
-    position absolute;
-    bottom 0;
-    width 100%;
-
+.app--footer {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+}
 </style>
