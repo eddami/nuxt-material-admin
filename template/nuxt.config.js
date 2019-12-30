e9eefdab7789350c2350e177af7cc286ae8faa28
Mohamed Eddami <eddamimo@gmail.com>
2019-12-30 22:25:20 +0100

    refactor: remove unused plugins

--- template/nuxt.config.js
+++ template/nuxt.config.js
@@ -1,70 +1,72 @@
-const pkg = require('./package')
+const pkg = require("./package");
 
-
-const VuetifyLoaderPlugin = require('vuetify-loader/lib/plugin')
+const VuetifyLoaderPlugin = require("vuetify-loader/lib/plugin");
 
 module.exports = {
-  mode: 'spa',
+  mode: "spa",
 
   /*
-  ** Headers of the page
-  */
+   ** Headers of the page
+   */
   head: {
-    title: 'Vue Material Admin Template',
+    title: "Vue Material Admin Template",
     meta: [
-      {charset: 'utf-8'},
-      {name: 'viewport', content: 'width=device-width, initial-scale=1'},
+      { charset: "utf-8" },
+      { name: "viewport", content: "width=device-width, initial-scale=1" },
       {
-        hid: 'description', name: 'description', content: 'Vue Material Admin Template is a \n' +
-          '    Google Material Design inspired admin dashboard template built with Vue and Vuetify.'
+        hid: "description",
+        name: "description",
+        content:
+          "Vue Material Admin Template is a \n" +
+          "    Google Material Design inspired admin dashboard template built with Vue and Vuetify."
       }
     ],
     link: [
-      {rel: 'icon', type: 'image/x-icon', href: '/favicon.ico'},
+      { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
       {
-        rel: 'stylesheet',
+        rel: "stylesheet",
         href:
-          'https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons'
+          "https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Material+Icons"
       }
     ],
     script: [
-      {src: 'https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.4/echarts-en.min.js'}
+      {
+        src:
+          "https://cdnjs.cloudflare.com/ajax/libs/echarts/4.0.4/echarts-en.min.js"
+      }
     ]
   },
 
   /*
-  ** Customize the progress-bar color
-  */
-  loading: {color: '#3adced'},
+   ** Customize the progress-bar color
+   */
+  loading: { color: "#3adced" },
 
   /*
-  ** Global CSS
-  */
+   ** Global CSS
+   */
   css: [
-    '~/assets/style/theme.styl',
-    '~/assets/style/app.styl',
-    'font-awesome/css/font-awesome.css',
-    'roboto-fontface/css/roboto/roboto-fontface.css'
+    "~/assets/style/theme.styl",
+    "~/assets/style/app.styl",
+    "font-awesome/css/font-awesome.css",
+    "roboto-fontface/css/roboto/roboto-fontface.css"
   ],
 
   /*
-  ** Plugins to load before mounting the App
-  */
-  plugins: [
-    '@/plugins/vuetify',
-    '@/plugins/vee-validate'
-  ],
+   ** Plugins to load before mounting the App
+   */
+  plugins: [],
 
   /*
-  ** Nuxt.js modules
-  */
+   ** Nuxt.js modules
+   */
   modules: [],
 
   /*
-  ** Build configuration
-  */
+   ** Build configuration
+   */
   build: {
-    transpile: ['vuetify/lib'],
+    transpile: ["vuetify/lib"],
     plugins: [new VuetifyLoaderPlugin()],
     loaders: {
       stylus: {
@@ -73,10 +75,8 @@ module.exports = {
     },
 
     /*
-    ** You can extend webpack config here
-    */
-    extend(config, ctx) {
-
-    }
+     ** You can extend webpack config here
+     */
+    extend(config, ctx) {}
   }
-}
+};
