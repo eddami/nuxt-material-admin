9aca4d786d4f90931ab3d06cddbe816a3f456e13
Mohamed Eddami <eddamimo@gmail.com>
2019-12-30 22:47:06 +0100

    fix: upgrade app drawer

--- template/components/AppDrawer.vue
+++ template/components/AppDrawer.vue
@@ -9,127 +9,166 @@
     width="260"
   >
     <v-toolbar color="primary darken-1" dark>
-      <img src="../static/m.png" height="36" alt="Vue Material Admin Template">
+      <img src="../static/m.png" height="36" alt="Vue Material Admin Template" />
       <v-toolbar-title class="ml-0 pl-3">
         <span class="hidden-sm-and-down">Vue Material</span>
       </v-toolbar-title>
     </v-toolbar>
     <vue-perfect-scrollbar class="drawer-menu--scroll" :settings="scrollSettings">
-      <v-list dense expand>
-        <template v-for="(item, i) in menus">
+      <v-list dense expend>
+        <template v-for="(item, i) in menuItems">
           <!--group with subitems-->
-          <v-list-group v-if="item.items" :key="item.name" :group="item.group" :prepend-icon="item.icon"
-                        no-action="no-action">
-            <v-list-tile slot="activator" ripple="ripple">
-              <v-list-tile-content>
-                <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-              </v-list-tile-content>
-            </v-list-tile>
+          <v-list-group
+            v-if="item.items"
+            :key="item.name"
+            :group="item.group"
+            :prepend-icon="item.icon"
+            no-action="no-action"
+          >
+            <template v-slot:activator>
+              <v-list-item-title>{{ item.title }}</v-list-item-title>
+            </template>
             <template v-for="(subItem, i) in item.items">
               <!--sub group-->
-              <v-list-group v-if="subItem.items" :key="subItem.name" :group="subItem.group" sub-group="sub-group">
-                <v-list-tile slot="activator" ripple="ripple">
-                  <v-list-tile-content>
-                    <v-list-tile-title>{{ subItem.title }}</v-list-tile-title>
-                  </v-list-tile-content>
-                </v-list-tile>
-                <v-list-tile v-for="(grand, i) in subItem.children" :key="i" :to="grand.href? grand.href : null"
-                             ripple="ripple">
-                  <v-list-tile-content>
-                    <v-list-tile-title>{{ grand.title }}</v-list-tile-title>
-                  </v-list-tile-content>
-                </v-list-tile>
+              <v-list-group
+                v-if="subItem.items"
+                :key="subItem.name"
+                :group="subItem.group"
+                sub-group="sub-group"
+              >
+                <template v-slot:activator>
+                  <v-list-item-title>{{ subItem.title }}</v-list-item-title>
+                </template>
+                <v-list-item
+                  v-for="(grand, i) in subItem.children"
+                  :key="i"
+                  :to="grand.href? grand.href : null"
+                >
+                  <v-list-item-title>{{ grand.title }}</v-list-item-title>
+                </v-list-item>
               </v-list-group>
               <!--child item-->
-              <v-list-tile v-else :key="i" :to="subItem.href? subItem.href : null"
-                           :disabled="subItem.disabled" :target="subItem.target" ripple="ripple">
-                <v-list-tile-content>
-                  <v-list-tile-title><span>{{ subItem.title }}</span></v-list-tile-title>
-                </v-list-tile-content>
-                <v-list-tile-action v-if="subItem.action">
+              <v-list-item
+                v-else
+                :key="i"
+                :to="subItem.href? subItem.href : null"
+                :disabled="subItem.disabled"
+                :target="subItem.target"
+              >
+                <v-list-item-title>
+                  <span>{{ subItem.title }}</span>
+                </v-list-item-title>
+                <v-list-item-action v-if="subItem.action">
                   <v-icon :class="[subItem.actionClass || 'success--text']">{{ subItem.action }}</v-icon>
-                </v-list-tile-action>
-              </v-list-tile>
+                </v-list-item-action>
+              </v-list-item>
             </template>
           </v-list-group>
           <v-subheader v-else-if="item.header" :key="i">{{ item.header }}</v-subheader>
           <v-divider v-else-if="item.divider" :key="i"></v-divider>
           <!--top-level link-->
-          <v-list-tile v-else :to="item.href ? item.href : null" ripple="ripple"
-                       :disabled="item.disabled" :target="item.target" rel="noopener" :key="item.name">
-            <v-list-tile-action v-if="item.icon">
+          <v-list-item
+            v-else
+            :to="item.href ? item.href : null"
+            :disabled="item.disabled"
+            :target="item.target"
+            rel="noopener"
+            :key="item.name"
+          >
+            <v-list-item-icon v-if="item.icon">
               <v-icon>{{ item.icon }}</v-icon>
-            </v-list-tile-action>
-            <v-list-tile-content>
-              <v-list-tile-title>{{ item.title }}</v-list-tile-title>
-            </v-list-tile-content>
-            <v-list-tile-action v-if="item.subAction">
-              <v-icon class="success--text">{{ item.subAction }}</v-icon>
-            </v-list-tile-action>
-          </v-list-tile>
+            </v-list-item-icon>
+            <v-list-item-title>{{ item.title }}</v-list-item-title>
+          </v-list-item>
         </template>
       </v-list>
     </vue-perfect-scrollbar>
   </v-navigation-drawer>
 </template>
 <script>
-  import menu from '@/api/menu';
-  import VuePerfectScrollbar from 'vue-perfect-scrollbar';
+import menu from '@/api/menu'
+import VuePerfectScrollbar from 'vue-perfect-scrollbar'
 
-  export default {
-    name: 'app-drawer',
-    components: {
-      VuePerfectScrollbar,
-    },
-    props: {
-      expanded: {
-        type: Boolean,
-        default: true
-      },
-    },
-    data: () => ({
-      mini: false,
-      menus: menu,
-      scrollSettings: {
-        maxScrollbarLength: 160
-      }
-    }),
-    computed: {
-      drawer: {
-        get() {
-          return this.$store.state.drawer
-        },
-        set(val) {
-          this.$store.commit('drawer', val)
-        }
-      },
-      computeGroupActive() {
-        return true;
-      },
-      sideToolbarColor() {
-        return this.$vuetify.options.extra.sideNav;
-      }
-    },
-    methods: {
-      genChildTarget(item, subItem) {
-        if (subItem.href) return;
-        if (subItem.component) {
-          return {
-            name: subItem.component,
-          };
-        }
-        return {name: `${item.group}/${(subItem.name)}`};
-      },
+export default {
+  name: 'app-drawer',
+  components: {
+    VuePerfectScrollbar
+  },
+  props: {
+    expanded: {
+      type: Boolean,
+      default: true
     }
-  };
+  },
+  data: () => ({
+    mini: false,
+    scrollSettings: {
+      maxScrollbarLength: 160
+    },
+    baseUrl:
+      process.env.NODE_ENV === 'development'
+        ? process.env.DEV_STATIC_BASE_URL
+        : process.env.PROD_STATIC_BASE_URL
+  }),
+  computed: {
+    drawer: {
+      get() {
+        return this.$store.state.drawer
+      },
+      set(val) {
+        this.$store.commit('drawer', val)
+      }
+    },
+    menuItems() {
+      const items = []
+      menu.forEach(item => {
+        let addIt = true
+        if (item.hasOwnProperty('forRoles')) {
+          addIt = false
+          for (let i = 0; i < this.user.roles.length; i++) {
+            const role = this.user.roles[i]
+            if (item.forRoles.indexOf(role) > -1) {
+              addIt = true
+              break
+            }
+          }
+        }
+        if (addIt) {
+          items.push(item)
+        }
+      })
+      return items
+    },
+    user() {
+      return this.$auth.user
+    }
+  },
+  methods: {
+    genChildTarget(item, subItem) {
+      if (subItem.href) return
+      if (subItem.component) {
+        return {
+          name: subItem.component
+        }
+      }
+      return { name: `${item.group}/${subItem.name}` }
+    }
+  }
+}
 </script>
 
 
 <style lang="stylus">
-  #appDrawer
-    overflow: hidden
-    .drawer-menu--scroll
-      height: calc(100vh - 48px)
-      overflow: auto
+#appDrawer {
+  overflow: hidden;
 
+  .drawer-menu--scroll {
+    height: calc(100vh - 48px);
+    overflow: auto;
+  }
+}
+
+.v-navigation-drawer__content {
+  overflow-y: hidden;
+}
 </style>
